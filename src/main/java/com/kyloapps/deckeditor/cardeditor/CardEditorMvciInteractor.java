e7f8de3ef09f7721253908b0e9c3fff749bab427
Renn <renntg@gmail.com>
2024-03-15 13:53:58 -0600

    Pre-overhaul of DEckEditorMvciInteractor

--- src/main/java/com/kyloapps/deckeditor/cardeditor/CardEditorMvciInteractor.java
+++ src/main/java/com/kyloapps/deckeditor/cardeditor/CardEditorMvciInteractor.java
@@ -1,8 +1,20 @@
 package com.kyloapps.deckeditor.cardeditor;
 
+import com.tobiasdiez.easybind.EasyBind;
+import javafx.beans.binding.Bindings;
+
 public class CardEditorMvciInteractor {
     private final CardEditorMvciModel model;
     public CardEditorMvciInteractor(CardEditorMvciModel model) {
         this.model = model;
+        bindTextFieldTileDirtyProperties();
+    }
+
+    /** Bind the getTextFieldTileDirtyProperties to the getTextFieldTiles mapped to their CompositeDirtyProperties */
+    private void bindTextFieldTileDirtyProperties() {
+        Bindings.bindContent(
+                model.getTextFieldTileDirtyProperties(),
+                EasyBind.mapBacked(model.getTextFieldTiles(), TextFieldTile::getCompositeDirtyProperty)
+        );
     }
 }
