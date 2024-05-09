033c427c9cfe44f366508fa1c3542aa6fd19446d
Renn Gilbert <renntg@gmail.com>
2024-05-09 15:23:55 -0600

    Added image displays, but there's a strange bug on fullscreen

--- src/main/java/com/kyloapps/practice/PracticeMvciInteractor.java
+++ src/main/java/com/kyloapps/practice/PracticeMvciInteractor.java
@@ -1,6 +1,9 @@
 package com.kyloapps.practice;
 
+import com.kyloapps.domain.Deck;
+import com.kyloapps.domain.Flashcard;
 import javafx.beans.binding.Bindings;
+import javafx.scene.image.Image;
 
 public class PracticeMvciInteractor {
 
@@ -10,12 +13,25 @@ public class PracticeMvciInteractor {
         this.model = model;
         manageNextPrevExists();
         bindQuestion();
+        bindImage();
+    }
+
+    private void bindImage() {
+        model.imageProperty().bind(Bindings.createObjectBinding(() -> {
+            if (model.getCurrentDeck() == null) return null;
+            String imageUrl = getCurrentFlashcard().getImageUrl();
+            return imageUrl == null || imageUrl.isBlank() ? null : new Image(imageUrl.strip(), true);
+        }, model.currentDeckProperty(), model.currentDeckIndexProperty()));
+    }
+
+    private Flashcard getCurrentFlashcard() {
+        return model.getCurrentDeck().getFlashcards().get(model.getCurrentDeckIndex());
     }
 
     private void bindQuestion() {
         model.questionProperty().bind(Bindings.createStringBinding(() -> {
             if (model.getCurrentDeck() == null) return "";
-            return model.getCurrentDeck().getFlashcards().get(model.getCurrentDeckIndex()).getQuestion();
+            return getCurrentFlashcard().getQuestion();
         }, model.currentDeckProperty(), model.currentDeckIndexProperty()));
     }
 
--- src/main/java/com/kyloapps/practice/PracticeMvciViewBuilder.java
+++ src/main/java/com/kyloapps/practice/PracticeMvciViewBuilder.java
@@ -6,12 +6,16 @@ import com.kyloapps.domain.Flashcard;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Builder;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -68,12 +72,11 @@ public class PracticeMvciViewBuilder implements Builder<Region> {
         rc.setPercentHeight(50);
         rootGridPane.getRowConstraints().add(rc);
 
+        questionPane.setGridLinesVisible(true);
+
         ColumnConstraints cc = new ColumnConstraints();
         cc.percentWidthProperty().bind(Bindings.createIntegerBinding(() -> model.getImage() == null ? 100 : 50, model.imageProperty()));
         questionPane.getColumnConstraints().add(cc);
-
-        rootGridPane.setGridLinesVisible(true);
-        questionPane.setGridLinesVisible(true);
 
         GridPane.setHgrow(questionPane, Priority.ALWAYS);
 
@@ -81,11 +84,106 @@ public class PracticeMvciViewBuilder implements Builder<Region> {
         GridPane.setVgrow(question, Priority.ALWAYS);
         GridPane.setHgrow(question, Priority.ALWAYS);
         questionPane.add(question, 0, 0);
+        Region image = createImagePane();
+        questionPane.add(image, 1,0);
 
         rootGridPane.add(questionPane, 0, 0);
 
         result.getChildren().add(rootGridPane);
         return result;
+    }
+
+    private Region createImagePane() {
+        Pane result = new Pane();
+        ImageView imageView = new ImageView();
+        imageView.setPreserveRatio(true);
+        imageView.imageProperty().bind(model.imageProperty());
+
+        imageView.fitWidthProperty().bind(result.widthProperty());
+
+        ChangeListener<Number> onLoadListener = (observable, oldProgress, newProgress) -> {
+            if (newProgress.doubleValue() == 1d) {
+                double imageRatio = getRatio(imageView);
+                if (result.getHeight() > predictHeight(result.getWidth(), imageRatio))
+                    imageView.fitWidthProperty().bind(result.widthProperty());
+                else
+                    imageView.fitHeightProperty().bind(result.heightProperty());
+            }
+        };
+
+        imageView.imageProperty().addListener((observable, oldImage, newImage) -> {
+            if (oldImage != null)
+                oldImage.progressProperty().removeListener(onLoadListener);
+            if (newImage != null)
+                newImage.progressProperty().addListener(onLoadListener);
+        });
+
+        result.widthProperty().addListener((observable, oldPaneWidth, newPaneWidth) -> {
+            double imageRatio = getRatio(imageView);
+            if (result.getHeight() > predictHeight(newPaneWidth.doubleValue(), imageRatio)) {
+                unbindImageView(imageView);
+                imageView.fitWidthProperty().bind(result.widthProperty());
+                System.out.println("width");
+            } else {
+                unbindImageView(imageView);
+                imageView.fitHeightProperty().bind(result.heightProperty());
+                System.out.println("height");
+            }
+        });
+
+        result.heightProperty().addListener((observable, oldPaneHeight, newPaneHeight) -> {
+            double imageRatio = getRatio(imageView);
+            if (result.getWidth() > predictWidth(newPaneHeight.doubleValue(), imageRatio)) {
+                unbindImageView(imageView);
+                imageView.fitHeightProperty().bind(result.heightProperty());
+                System.out.println("height");
+            } else {
+                unbindImageView(imageView);
+                imageView.fitWidthProperty().bind(result.widthProperty());
+                System.out.println("width");
+            }
+        });
+
+        ProgressIndicator loadingIndicator = new ProgressIndicator();
+
+        imageView.imageProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) return;
+            loadingIndicator.progressProperty().unbind();
+            loadingIndicator.progressProperty().bind(Bindings.createDoubleBinding(() -> {
+                    return newValue.isBackgroundLoading() ? -1d : 0d;
+            }, newValue.progressProperty()));
+
+        });
+
+        StackPane stackPane = new StackPane(loadingIndicator, imageView);
+        stackPane.setAlignment(Pos.CENTER);
+        stackPane.prefWidthProperty().bind(result.widthProperty());
+        stackPane.prefHeightProperty().bind(result.heightProperty());
+        stackPane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        result.getChildren().add(stackPane);
+
+        GridPane.setHgrow(result, Priority.ALWAYS);
+        return result;
+    }
+
+    private static void unbindImageView(ImageView imageView) {
+        imageView.fitWidthProperty().unbind();
+        imageView.fitHeightProperty().unbind();
+    }
+
+    // imageRatio = width / height
+    private double predictHeight(double width, double imageRatio) {
+        return width / imageRatio;
+    }
+
+    // imageRatio = width / height
+    private double predictWidth(double height, double imageRatio) {
+        return imageRatio * height;
+    }
+
+    private double getRatio(ImageView imageView) {
+        if (imageView.getImage() == null) return 0;
+        return imageView.getImage().getWidth() / imageView.getImage().getHeight();
     }
 
     private Node createQuestionLabel() {
